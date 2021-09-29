7c901bc86fa9b27679b601b9fcfbdd0daec3407a
Jack Carpenter <jackcarpenter1@live.com>
2021-09-29 14:16:09 -0500

    Further update

--- Develop/script.js
+++ Develop/script.js
@@ -7,77 +7,87 @@
 //Global Variables
 var saveBtn = $(".saveBtn");
 var timeDisplay = $("#currentDay");
-var now = moment().format('MMMM Do YYYY, h:mm:ss a');
+var now = moment();
+var currentHour = moment().format("LT");
 var textArea = $(".activityText");
 var activityEntry;
-var allNotes = [
-    {
-        timeSlot: "9:00:00 am",
-        activityLog: textArea,
-        button: saveBtn,
-    },
-    {
-        timeSlot: "10:00:00 am",
-        activityLog: textArea,
-        button: saveBtn,
-    },
-    {
-        timeSlot: "11:00:00 am",
-        activityLog: textArea,
-        button: saveBtn,
-    },
-    {
-        timeSlot: "12:00:00 pm",
-        activityLog: textArea,
-        button: saveBtn,
-    },
-    {
-        timeSlot: "1:00:00 pm",
-        activityLog: textArea,
-        button: saveBtn,
-    },
-    {
-        timeSlot: "2:00:00 pm",
-        activityLog: textArea,
-        button: saveBtn,
-    },
-    {
-        timeSlot: "3:00:00 pm",
-        activityLog: textArea,
-        button: saveBtn,
-    },
-    {
-        timeSlot: "4:00:00 pm",
-        activityLog: textArea,
-        button: saveBtn,
-    },
-    {
-        timeSlot: "5:00:00 pm",
-        activityLog: textArea,
-        button: saveBtn,
-    }
-];
+var allNotes = ["", "", "", "", "", "", "", "", ""];
+
+
+// Might need, later
+// JSON.stringify
+
+
+
+// For determining past/present/future
+// $("input").each(function(){
+//     var dataHour = $(this).attr("data-hour")
+//     console.log(dataHour);
+//   Color coding
+//     if(data-hour > currentHour) {
+//     addClass("future")
+//     }
+//     if else(data-hour < currentHour) {
+//     addClass("past")    
+//     }
+//     else {
+//         addClass("present")
+//     }
+
+// })
+
+
 
 //Displaying Time on Loading page
 timeDisplay.text(now)
 
+
+
 // Event Listener for saving activities
 saveBtn.on("click", function(){
-    debugger
-    activityEntry = textArea.val()
+    // use $(this) to get the button that was clicked, and traverse the dom to find the sibling textarea
+    selectedInput = $(this).closest(".row").children("input")
+    localStorage.setItem(allNotes, selectedInput.val())
+    
+    // console.log( $(this).closest(".row").children("input") );
+
+
+    // figure out based on the timeblock what item in the array should be updated
+    // update just that item 
+    // save the entire array to local storage
+
+
+    // activityEntry = textArea.val()
+    // console.log(activityEntry)
     // localStorage.setItem("Activity", textArea.val())
-    // textArea.val(localStorage.getItem("Activity"))
+    //textArea.val(localStorage.getItem("Activity"))
     // if ((this)===allNotes.button){
     //     localStorage.setItem("Activity", textArea.val())
     //     textArea.val(localStorage.getItem("Activity"))
     //     .closest(allNotes.activityLog)
-        //Above item is not being detected in debugger, format adjustment required
+        // Above item is not being detected in debugger, format adjustment required
     }
-//}
 )
 
-//Recording activity on corresponding slot
+// Color coding
+// if(data-hour>now) {
+//     addClass("future")
+// }
+//     if else (data-hour<now) {
+//     addClass("past")    
+//     }
+//     else {
+//         addClass("present")
+//     }
+
+function initializa(){
+
+}
+
+
 function recordActivity() {
 
 }
+
+
 
